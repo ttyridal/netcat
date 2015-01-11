dad86e53f0ebf5bdddc57923433e741e0166d4d6
brock <laoyaogg@qq.com>
2015-01-11 09:07:42 +0800

    indent

--- generic.h
+++ generic.h
@@ -1,21 +1,21 @@
 /* generic.h -- anything you don't #undef at the end remains in effect.
-   The ONLY things that go in here are generic indicator flags; it's up
-   to your programs to declare and call things based on those flags.
+The ONLY things that go in here are generic indicator flags; it's up
+to your programs to declare and call things based on those flags.
 
-   You should only need to make changes via a minimal system-specific section
-   at the end of this file.  To build a new section, rip through this and
-   check everything it mentions on your platform, and #undef that which needs
-   it.  If you generate a system-specific section you didn't find in here,
-   please mail me a copy so I can update the "master".
+You should only need to make changes via a minimal system-specific section
+at the end of this file.  To build a new section, rip through this and
+check everything it mentions on your platform, and #undef that which needs
+it.  If you generate a system-specific section you didn't find in here,
+please mail me a copy so I can update the "master".
 
-   I realize I'm probably inventing another pseudo-standard here, but
-   goddamnit, everybody ELSE has already, and I can't include all of their
-   hairball schemes too.  HAVE_xx conforms to the gnu/autoconf usage and
-   seems to be the most common format.  In fact, I dug a lot of these out
-   of autoconf and tried to common them all together using "stupidh" to
-   collect data from platforms.
+I realize I'm probably inventing another pseudo-standard here, but
+goddamnit, everybody ELSE has already, and I can't include all of their
+hairball schemes too.  HAVE_xx conforms to the gnu/autoconf usage and
+seems to be the most common format.  In fact, I dug a lot of these out
+of autoconf and tried to common them all together using "stupidh" to
+collect data from platforms.
 
-   In disgust...  _H*  940910, 941115.  Pseudo-version: 1.1  */
+In disgust...  _H*  940910, 941115.  Pseudo-version: 1.1  */
 
 #ifndef GENERIC_H		/* only run through this once */
 #define GENERIC_H
@@ -25,7 +25,7 @@
 /* =============================== */
 
 /* How does your system declare malloc, void or char?  Usually void, but go
-   ask the SunOS people why they had to be different... */
+ask the SunOS people why they had to be different... */
 #define VOID_MALLOC
 
 /* notably from fwtk/firewall.h: posix locking? */
@@ -42,8 +42,8 @@
 #define HAVE_LSTAT
 
 /* different kinds of term ioctls.  How to recognize them, very roughly:
-   sysv/POSIX_ME_HARDER:  termio[s].h, struct termio[s], tty.c_*[]
-   bsd/old stuff:  sgtty.h, ioctl(TIOCSETP), sgttyb.sg_*, tchars.t_*
+sysv/POSIX_ME_HARDER:  termio[s].h, struct termio[s], tty.c_*[]
+bsd/old stuff:  sgtty.h, ioctl(TIOCSETP), sgttyb.sg_*, tchars.t_*
 #define HAVE_TERMIOS */
 
 /* dbm vs ndbm */
@@ -65,7 +65,7 @@
 /* ============= */
 
 /* Presence of these can be determined via a script that sniffs them
-   out if you aren't sure. */
+out if you aren't sure. */
 
 /* stdlib comes with most modern compilers, but ya never know */
 #define HAVE_STDLIB_H
@@ -96,9 +96,9 @@
 /* ==================== */
 
 /* Still maybe have to do something about the following, if it's even
-   worth it.  I just grepped a lot of these out of various code, without
-   looking them up yet:
-	   
+worth it.  I just grepped a lot of these out of various code, without
+looking them up yet:
+
 #define HAVE_EINPROGRESS
 #define HAVE_F_SETOWN
 #define HAVE_SETENV ... now *there's* a hairy one; **environ is portable
@@ -110,10 +110,10 @@
 #define HAVE_ST_BLKSIZE ... a stat() thing?
 #define HAVE_IP_TOS
 #define HAVE_STRFTIME ... screw this, we should just INCLUDE one for lame
-   old boxes that don't have it [sunos 3.x, early 4.x?]
+old boxes that don't have it [sunos 3.x, early 4.x?]
 #define HAVE_VFPRINTF
 #define HAVE_SHADOW_PASSWD  ... in its multitudinous schemes?? ... how
-   about sumpin' like #define SHADOW_PASSWD_TYPE ... could get grody.
+about sumpin' like #define SHADOW_PASSWD_TYPE ... could get grody.
 #define SIG*  ... what a swamp, punt for now; should all be in signal.h
 #define HAVE_STRCSPN  ... see larry wall's comment in the fwtk regex code
 #define ULTRIX_AUTH  ... bwahaha.
@@ -130,7 +130,7 @@ There's a BIG swamp lurking where network code of any sort lives.
 /* ======================== */
 
 /* By turning OFF various bits of the above,  you can customize for
-   a given platform.  */
+a given platform.  */
 
 /* DOS boxes, with MSC; you may need to adapt to a different compiler. */
 #ifdef MSDOS
@@ -181,7 +181,7 @@ There's a BIG swamp lurking where network code of any sort lives.
 #endif /* aix */
 
 /* linux, which is trying as desperately as the gnu folks can to be
-   POSIXLY_CORRECT.  I think I'm gonna hurl... */
+POSIXLY_CORRECT.  I think I'm gonna hurl... */
 #ifdef LINUX
 #undef UTMPX
 #undef HAVE_SYSINFO
